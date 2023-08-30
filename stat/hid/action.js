e52859ac583e88d543e5935d0492f82cda3ced3d
Mischka van Wyk <m.vanwyk@lab.clicklearning.org>
2023-08-30 21:19:37 +0200

    init commit

---
+++ stat/hid/action.js
@@ -0,0 +1,13 @@
+const add = {
+    type: "ADD",
+  };
+  
+  const subtract = {
+    type: "SUBTRACT",
+  };
+  
+  const reset = {
+    type: "RESET",
+  };
+  
+  export { add, subtract, reset };
---
+++ stat/hid/red.js
@@ -0,0 +1,14 @@
+function reducer(state, action) {
+    switch (action.type) {
+      case "ADD":
+        return { count: state.count + 1 };
+      case "SUBTRACT":
+        return { count: state.count - 1 };
+      case "RESET":
+        return { count: 0 };
+      default:
+        return state;
+    }
+  }
+  
+  export { reducer };
